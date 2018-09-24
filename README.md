944cfe5600157748777b55af7db8d9a9dbe29a6f
moharilstackup <moharilstackup@gmail.com>
2018-09-24 16:35:48 +0800

    completed day6 ssf

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # projectBase
+
+CREATE  TABLE `books` (
+  `idbooks` INT NOT NULL AUTO_INCREMENT ,
+  `name` TEXT NOT NULL ,
+  `author` VARCHAR(100) NOT NULL ,
+  `publish_year` YEAR NOT NULL ,
+  `isbn` BIGINT(13) UNSIGNED NOT NULL ,
+  PRIMARY KEY (`idbooks`) ,
+  UNIQUE INDEX `idbooks_UNIQUE` (`idbooks` ASC) );
+
+````sql
+SELECT * from books
+
+INSERT INTO books (name, author, publish_year, isbn) VALUES ('Harry Potter and the Prisoner of Azkaban', 'JK Rowling', 1996, 9780439655484) 
+INSERT INTO books (name, author, publish_year, isbn) VALUES ('Harry Potter and the Philosopher\'s Stone', 'JK Rowling', 1997, 9780439554930) 
+INSERT INTO books (name, author, publish_year, isbn) VALUES ('Harry Potter and the Goblet of Fire', 'JK Rowling', 1996, 9780439655485); 
+INSERT INTO books (name, author, publish_year, isbn) VALUES ('Harry Potter and the Goblet of Fire 2', 'JK Rowling', 1996, 9780439655486); 
+INSERT INTO books (name, author, publish_year, isbn) VALUES ('Harry Potter and the Goblet of Fire3', 'JK Rowling', 1996, 9780439655487);
+INSERT INTO books (name, author, publish_year, isbn) VALUES ('Harry Potter and the Goblet of Fire4', 'JK Rowling', 1996, 9780439655488);
+````
--- main.js
+++ main.js
@@ -1,14 +1,111 @@
 //Step 1: load path and express
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const hbs = require("express-handlebars");
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const uuidv1 = require('uuid/v1');
 const mysql = require("mysql");
+const q = require("q");
+
+
+
+//Step 2: create an instance of the application
+const app = express();
+
+const sqlFindAllFilms = "SELECT * FROM film limit 1";
+const sqlFindOneBook = "SELECT name, author, publish_year, isbn FROM books WHERE idbooks=?";
+
+console.log("process.env.DB_PORT => ",process.env.DB_PORT);
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST, //"localhost",
+    port: process.env.DB_PORT, //3306,
+    user: process.env.DB_USER, //"root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME, //"derby",
+    connectionLimit: process.env.DB_CONLIMIT //4
+    // debug: true
+});
+
+var makeQuery = (sql, pool) => {
+    console.log(sql);
+    // var defer = q.defer();
+    return (args) => {
+        var defer = q.defer();
+        pool.getConnection((err, connection) => {
+            if (err) {
+                defer.reject(err);
+                return;
+            }
+            connection.query(sql, args || [], (err, results) => {
+                connection.release();
+                if (err) {
+                    defer.reject(err);
+                    return;
+                }
+                defer.resolve(results);
+            })
+        });
+        return defer.promise;
+    }
+
+}
+
+var findAllFilms = makeQuery(sqlFindAllFilms, pool);
+var findOneBookById = makeQuery(sqlFindOneBook, pool);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/films", (req, res) => {
+    findAllFilms().then((results)=>{
+        res.json(results);
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).json(error); //.end()
+    });
+    // res.json({ });
+});
+//sample test : http://localhost:3000/books/3
+app.get("/books/:bookId", (req,res) => {
+    var bookId=req.params.bookId;
+    console.log(bookId);
+    findOneBookById([bookId]).then((results)=>{
+        res.json(results);
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).json(error); //.end()
+    });
+    // res.json({});
+});
+
+//sample test : http://localhost:3000/books?bookId=1
+app.get("/books", (req,res) => {
+    console.log("/books query !");
+    var bookId=req.query.bookId;
+    console.log(bookId);
+    findOneBookById([bookId]).then((results)=>{
+        res.json(results);
+    }).catch((error)=>{
+        console.log(error);
+        res.status(500).json(error); //.end()
+    });
+    // res.json({});
+});
+
+//Step 4: start the server
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
+
+app.listen(PORT, () => {
+    console.info(`Application started on port ${PORT} at ${new Date()}`);
+}
+);
+
 
 //Configure a connection pool to the database
-const pool = mysql.createPool({
+/* const pool = mysql.createPool({
     host: "localhost",
     port: 3306,
     user: "fred",
@@ -16,20 +113,20 @@ const pool = mysql.createPool({
     database: "derby",
     connectionLimit: 4
 });
-
+ */
 
 //Step 2: create an instance of the application
-const app = express();
+// const app = express();
 
 //Setup views
-app.engine('hbs', hbs({ defaultLayout: 'main.hbs' }))
-app.set('view engine', 'hbs');
+/* app.engine('hbs', hbs({ defaultLayout: 'main.hbs' }))
+app.set('view engine', 'hbs'); */
 
 
 //Step 3: define routes
 //GET UUID
 // uuidv1(); // ⇨ '45745c60-7b1a-11e8-9c9c-2d42b21b1a3e'
-app.get('/uuid', (req, resp) => {
+/* app.get('/uuid', (req, resp) => {
     const uuid = uuidv1();
     resp.status(200);
     resp.format({
@@ -52,16 +149,16 @@ app.get('/uuid', (req, resp) => {
         }
     });
 
-});
+}); */
 
 
 //Serves from public
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 //Step 4: start the server
-const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
+// const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
 
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
     console.info(`Application started on port ${PORT} at ${new Date()}`);
 }
-);
+); */
